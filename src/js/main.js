401b3bcb31ed4a2ff194ec2bf6baf8d0ebe93804
nechiporenko <s.nechiporenko@gmail.com>
2015-11-16 21:26:34 +0200

    tabs & terms page created

--- src/js/main.js
+++ src/js/main.js
@@ -74,6 +74,8 @@ jQuery(document).ready(function ($) {
         notice.fadeOut('slow');
     });
 
+
+
     //scroll to section (link on Product Page menu)
     $('.blog-header').on('click', '[data-scroll]', function (e) {
         e.preventDefault();
@@ -84,4 +86,45 @@ jQuery(document).ready(function ($) {
             }, 800);
         }
     });
+
+
+
+    //tabs init
+    $('.tabs').each(function () {
+        var current = $(this).find('.tabs__link.active');
+        if (current.length < 1) { current = $(this).find('li:first').find('a').addClass('active'); }
+        current = $(this).find('.tabs__link.active').attr('href');
+        $(current).fadeIn();
+    });
+    //tabs click
+    $('.tabs').on('click', '.tabs__link', function (e) {
+        e.preventDefault();
+        var $tabs = $(this).parents('ul');
+        if ($(this).hasClass('active')) {
+            return false;
+        } else {
+            var tab_current = $tabs.find('a.active').attr('href'),
+                target = $(this).attr('href');
+            $tabs.find('a').removeClass('active');
+            $(tab_current).hide();
+            $(this).addClass('active');
+            $(target).fadeIn();
+            history.pushState(null, null, window.location.search + $(this).attr('href'));
+        }
+    });
+
+    // tab hashtag identification and auto-focus
+    var wantedTag = window.location.hash;
+    if (wantedTag != "") {
+        try {
+            var $allTabs = $(".tabs a[href^=" + wantedTag + "]").parents('.tabs').find('a');
+            var defaultTab = $allTabs.filter('.active').attr('href');
+            $(defaultTab).hide();
+            $allTabs.removeClass('active');
+            $(".tabs a[href^=" + wantedTag + "]").addClass('active');
+            $("#" + wantedTag.replace('#', '')).show();
+        } catch (e) {
+            // I have no idea what to do here, so I'm leaving this for the maintainer.
+        }
+    }
 });
